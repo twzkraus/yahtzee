43ef2d951fff39d1f087f1e3badc5d4619818444
twzkraus <twzkraus@gmail.com>
2020-11-20 16:29:28 -0800

    updates name of game and cleans up formatting for titles on scorecard

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const prettyNames = {
+    'ones': 'Ones',
+    'twos': 'Twos',
+    'threes': 'Threes',
+    'fours': 'Fours',
+    'fives': 'Fives',
+    'sixes': 'Sixes',
+    'chance': 'Chance',
+    '3ok': '3 of a Kind',
+    '4ok': '4 of a Kind',
+    'fullHouse': 'Full House',
+    'smallStraight': 'Small Straight',
+    'largeStraight': 'Large Straight',
+    'yahtzee': 'Yahtzee',
+};
+
 const ScoreCard = ({ scores }) => {
   let scoreArray = [];
   for (let key in scores) {
@@ -8,7 +24,7 @@ const ScoreCard = ({ scores }) => {
 
   const getRow = (arrayEl) => (
     <tr>
-      <td>{arrayEl[0]}</td>
+      <td>{prettyNames[arrayEl[0]]}</td>
       <td>{arrayEl[1]}</td>
     </tr>
   );
