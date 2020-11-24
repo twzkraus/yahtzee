38f85c9020e5ba835ddf3747c5be5e504c9b8e4f
twzkraus <twzkraus@gmail.com>
2020-11-24 14:35:15 -0800

    updates scoreform to show better formatted score titles

--- src/components/ScoreForm.jsx
+++ src/components/ScoreForm.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ScoreItem from './ScoreItem.jsx';
 
+const prettyNames = {
+  'ones': 'Ones',
+  'twos': 'Twos',
+  'threes': 'Threes',
+  'fours': 'Fours',
+  'fives': 'Fives',
+  'sixes': 'Sixes',
+  'chance': 'Chance',
+  '3ok': '3 of a Kind',
+  '4ok': '4 of a Kind',
+  'fullHouse': 'Full House',
+  'smallStraight': 'Small Straight',
+  'largeStraight': 'Large Straight',
+  'yahtzee': 'Yahtzee',
+};
+
 const ScoreForm = ({ scores, handleFormSubmit }) => {
   let scoreArray = [];
   for (let key in scores) {
     if (scores[key]) {
-      scoreArray.push([scores[key], key]);
+      scoreArray.push([scores[key], prettyNames[key]]);
     }
   }
   return (
