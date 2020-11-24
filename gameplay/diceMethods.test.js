334791d1075ac559e861b4d981dd686f1ceac06c
twzkraus <twzkraus@gmail.com>
2020-11-24 10:28:31 -0800

    adds tests for small straight & large straight. Dice Methods properly identify them.

--- gameplay/diceMethods.test.js
+++ gameplay/diceMethods.test.js
@@ -12,8 +12,27 @@ test('properly identifies a 4 of a kind', () => {
 });
 
 test('properly identifies a small straight', () => {
-  let hand = [5, 4, 2, 3, 2];
-  let result = play.getAllScores(hand);
-  expect(result['smallStraight']).toBeGreaterThan(0);
+  for (let first = 1; first <= 3; first++) {
+    let hand = [first];
+    while (hand.length < 4) {
+      hand.push(first + hand.length);
+    }
+    for (let j = 1; j <= 6; j++) {
+      hand.push(j);
+      let result = play.getAllScores(hand);
+      expect(result['smallStraight']).toBe(30);
+    }
+  }
+});
+
+test('properly identifies a large straight', () => {
+  for (let first = 1; first <= 2; first++) {
+    let hand = [first];
+    while (hand.length < 5) {
+      hand.push(first + hand.length);
+    }
+    let result = play.getAllScores(hand);
+    expect(result['largeStraight']).toBe(40);
+  }
 });
 
