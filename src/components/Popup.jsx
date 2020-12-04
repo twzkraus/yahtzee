752ae346d0e78038d26928ec1d9b4ccbd850d72f
twzkraus <twzkraus@gmail.com>
2020-12-03 19:44:08 -0800

    begins to implement react portal for end of game popup. Adds some basic styling. Doesn't look very good yet.

---
+++ src/components/Popup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Popup = ({ message }) => {
+  console.log('pop pop!!');
+  return (
+  <div>
+    <h1>
+      {message}
+    </h1>
+  </div>
+)};
+
+export default Popup;
