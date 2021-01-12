31ab831e2021b5c011445bc8df42a5cd8ab8a442
twzkraus <twzkraus@gmail.com>
2021-01-12 14:25:51 -0800

    initializes game with popup, which then controls the number of players on the board

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Popup = ({ scenario, winner }) => {
+const Popup = ({ scenario, winner, startNewGame }) => {
+
+  const [numPlayers, setNumPlayers] = useState(2);
+
+  const changeNumPlayers = (direction) => {
+    if (direction === '-' && numPlayers > 1) {
+      setNumPlayers(numPlayers - 1);
+    } else if (direction ==='+') {
+      setNumPlayers(numPlayers + 1);
+    }
+  }
 
   const getStartingPopup = () => {return (
     <>
       <h1>
-        {`Welcome. How many players?`}
+        {`Welcome Aboard! How many players are joining us?`}
       </h1>
+      <br></br>
+      <h1><button onClick={() => changeNumPlayers('-')}>{'-'}</button>
+        {numPlayers}
+        <button onClick={() => changeNumPlayers('+')}>{'+'}</button>
+        </h1>
+        <button onClick={() => startNewGame(numPlayers)}>{'OK!'}</button>
     </>
   )};
 
